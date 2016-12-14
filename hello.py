ad12459f21db933a326cf7a2d34fb83c1df6b349
Nina Mik <nina.mikusova@gmail.com>
2016-12-14 12:06:41 +0200

    prints whatever you tell it to

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
-# a different comment
-# and a comment to master
-def say_hello():
-    print("Lunch, world!")
-    print("Bye Bye World!")
+def say_something(something):
+    if something:
+        print(" ".join(something))
+    else:
+        print("Hello World!")
